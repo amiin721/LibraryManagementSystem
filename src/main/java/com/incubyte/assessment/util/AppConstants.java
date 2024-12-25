4640689016a6a523d07503772e684cb43baca882
tntra-amin <amiin.tntra@gmail.com>
2024-12-26 00:23:02 +0530

    Added AppConstants class to hold commonly used message templates and error messages

---
+++ src/main/java/com/incubyte/assessment/util/AppConstants.java
@@ -0,0 +1,20 @@
+package com.incubyte.assessment.util;
+
+/**
+ * Utility class that holds constant values used throughout the library system.
+ * This class provides commonly used message templates and error messages to ensure consistency
+ * across the application. It avoids hardcoding values throughout the codebase, making it easier to
+ * manage and update messages.
+ */
+public class AppConstants {
+
+    public static final String BOOK_MESSAGE_PREFIX = "Book with ISBN %s";
+
+    //Library Management Error Constants
+    public final static String BOOK_ALREADY_EXISTS = BOOK_MESSAGE_PREFIX + " already exists in the library.";
+    public final static String INVALID_ISBN = "ISBN cannot be null or empty.";
+    public final static String INVALID_TITLE = "Title cannot be null or empty.";
+    public final static String INVALID_AUTHOR = "Author cannot be null or empty.";
+    public final static String INVALID_PUBLICATION_YEAR = "Invalid publication year.";
+
+}
